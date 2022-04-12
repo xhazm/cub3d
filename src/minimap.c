d99e46814352f1307032ccb3eba1a52488502040
Sir Com <lars.pfleiderfb@gmail.com>
2022-04-12 16:26:57 +0200

    fixed: better minimap player tracking:

--- src/minimap.c
+++ src/minimap.c
@@ -9,8 +9,8 @@ static void	ft_minimap_player(t_vars *vars,t_map *map_info)
 	y = -(MINIMAP_S / 2);
 	while (y < MINIMAP_S / 2)
 	{
-		x = 0;
-		while (x < MINIMAP_S)
+		x = -(MINIMAP_S / 2);
+		while (x < MINIMAP_S / 2)
 		{
 			my_mlx_pixel_put(vars, (int)(vars->pl.x * MINIMAP_S) + x,
 				(int)(vars->pl.y * MINIMAP_S) + y, 0x00ff0000);
@@ -38,7 +38,6 @@ void	ft_minimap(t_vars *vars, t_map *map_info)
 				my_mlx_pixel_put(vars, x, y, 0x00ff9933);
 			else if(map_info->map[y / MINIMAP_S][x / MINIMAP_S] == -'R')
 				my_mlx_pixel_put(vars, x, y, 0x00ffffff);
-
 			x++;
 		}
 		y++;
